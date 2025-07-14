baf6b291acae0fab08cd12caa5e3097096526a4d
Aziz <azizhoumani@gmail.com>
2025-07-14 12:16:06 +0200

    creation d_un layout racine

---
+++ src/app/dashboards/layout.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+function layout({children}) {
+  return (
+    <div>
+      <nav className="flex gap-x-2">
+        <a className="underline" href="/dashboards/entreprise">Dashboard finance</a>
+        <a className="underline" href="/dashboards/rh">Dashboard RH</a>
+      
+    </nav>
+    {children}
+    </div>
+  )
+
+}
+
+export default layout
