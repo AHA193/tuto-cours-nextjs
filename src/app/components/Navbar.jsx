d9371a4dc7a380cdecfb098bd4dfdffb6830905d
Aziz <azizhoumani@gmail.com>
2025-07-14 14:11:06 +0200

    routes dynamiques 1 _affichage de tous les posts dans la section blog

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,14 +4,14 @@ import { usePathname } from "next/navigation"
 
 function Navbar() {
     const pathname = usePathname()
-    console.log(pathname);
+    // console.log(pathname);
 
   return (
     <nav className="flex gap-x-2">
         <Link className="underline" href="/">Accueil</Link>
         <Link className="underline" href="/blog">Blog</Link>
         <Link className="underline" href="/dashboards">Dashboards</Link>
-        <Link className={`underline ${pathname === "/contact" && "bg-red-600" : ""}`} href="/contact">Contact</Link>
+        <Link className={`underline ${pathname === "/contact" && "bg-red-600" }`} href="/contact">Contact</Link>
     </nav>
   )
 }
