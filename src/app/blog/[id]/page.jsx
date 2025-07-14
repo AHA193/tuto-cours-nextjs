3c2fce55fbc3cb5d31330266b8a3fcd255810ef8
Aziz <azizhoumani@gmail.com>
2025-07-14 15:58:14 +0200

    Route dynamique 2 _seg dynam pour retrouver ressource lien sur titre vers texte article

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
 
-function page() {
+
+async function page({ params }) {
+  const { id } = await params
+  console.log(id);
+
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+  const post = await res.json()
+  console.log(post);
+
   return (
-    <div>
-      <div>page</div>
-    </div>
+    <main className='mt-12'>
+      <h1 className='text-3-xl font-bold mb-4'>{post.title}</h1>
+      <p className='text-gray-600 mb-8'>{post.body}</p>
+
+      <Link href="/blog" className="text-blue-500 hover:underline">
+        ← Retour à la page de blog</Link>
+    </main>
   )
 }
 
 export default page
- 
